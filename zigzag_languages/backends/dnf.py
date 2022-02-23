0ce13425fbd1e8aee4492ba9f43c6b7d273375e7
Maciek Krawiec <142805+mkrawiec@users.noreply.github.com>
2022-02-23 23:06:57 +0100

    Make dnf backend more accurate

--- zigzag_languages/backends/dnf.py
+++ zigzag_languages/backends/dnf.py
@@ -1,23 +1,38 @@
 from ..commands.dnf import run_dnf
 from .base import PackageBackend, AvailableBackends
 
+COMMON_ARGS = (
+    "--cacheonly",
+    "repoquery-n",
+    "--qf",
+    "%{name}",
+)
+
 
 class DnfBackend(PackageBackend):
     def get_backend_name(self):
         return AvailableBackends.DNF
 
     def get_language_packages(self, installed_packages):
+        provided = self._get_provided_packages()
         recommended = run_dnf(
-            "--cacheonly",
-            "repoquery-n",
-            "--qf",
-            "%{name}",
+            *COMMON_ARGS,
             "--latest-limit=1",
             "--whatsupplements",
-            ",".join(installed_packages),
+            ",".join(provided),
             "*-lang",
         )
         return recommended.difference(installed_packages)
 
     def install_new(self, language_packages):
         run_dnf("install", "-y", *language_packages)
+
+    def _get_provided_packages(self):
+        """get all provided package-like capabilities"""
+        all_capabilities = run_dnf(*COMMON_ARGS, "--provides", "--installed")
+
+        return {
+            line.split(" ")[0]
+            for line in all_capabilities
+            if "=" in line and "(" not in line
+        }
